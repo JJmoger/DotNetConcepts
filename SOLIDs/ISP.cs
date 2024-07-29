3534d3ca1ebde78c240c8677201314db9bc924f3
Sanjay Kumar <imsanjayk95@gmail.com>
2024-07-29 16:09:02 +0530

    solids

---
+++ SOLIDs/ISP.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLIDs
+{
+    internal class ISP
+    {
+        /* Interface Segregation Principle - ISP
+         * A class should not be forced to depend on interfaces they do not use.
+         * It can be implemented using Multiple inheritance. 
+         * 
+         */
+
+        //Violates ISP
+        interface IWorker
+        {
+            void Work();
+            void Eat();
+            void Run()
+            {
+
+            }
+        }
+
+        class Human : IWorker
+        {
+            public void Eat()
+            {
+                Console.WriteLine("Human eat");
+            }
+
+            public void Work()
+            {
+                Console.WriteLine("work");
+            }
+        }
+
+        class Robot : IWorker
+        {
+            public void Eat()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Work()
+            {
+                Console.WriteLine("work");
+            }
+        }
+
+        interface IWorkForce
+        {
+            void Workable();
+        }
+
+        interface IFeedable
+        {
+            void Feed();
+        }
+
+        class Man : IWorkForce, IFeedable
+        {
+            public void Feed()
+            {
+                Console.WriteLine("eat");
+            }
+
+            public void Workable()
+            {
+                Console.WriteLine("work");
+            }
+        }
+
+        class Machine: IWorkForce
+        {
+            public void Workable()
+            {
+                Console.WriteLine("work");
+            }
+        }
+    }
+}
---
+++ SOLIDs/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace SOLIDs
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            // SRP
+            var user = new User();
+            user.Name = "Sanjay";
+            user.Email = "email@gmail.com";
+
+            var repos = new UserRepository();
+            repos.SaveUser(user);
+
+            var emails = new EmailService();
+            emails.SendEmail(user.Email);
+
+            // OCP
+            IReport pdf = new PDFReports();
+            IReport word = new WordReports();
+            ReportExtension report = new ReportExtension(pdf);
+            report.GenerateReport();
+
+            report = new ReportExtension(word);
+            report.GenerateReport();
+
+            // LSP
+          
+
+
+
+        }
+        
+    }
+}
