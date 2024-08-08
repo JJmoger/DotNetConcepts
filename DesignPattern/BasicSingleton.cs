0c011d5d004e4fd1c502dd57d09d3a0e194edd94
Sanjay Kumar <imsanjayk95@gmail.com>
2024-08-08 19:26:19 +0530

    Design pattern

---
+++ DesignPattern/BasicSingleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    internal class BasicSignleton
+    {
+        private static BasicSignleton _instance;
+        private static readonly object _lock = new object();
+
+        private BasicSignleton(){}
+
+        public static BasicSignleton Instance {  get 
+            {
+                lock (_lock)
+                {
+                    if ( _instance == null) 
+                    { 
+                        _instance = new BasicSignleton();
+                    }
+                    return _instance;
+                }
+            } 
+        }
+    }
+}
---
+++ StaticSingleton/Program.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace StaticSingleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "jj100klom300lklk99"
+            var str = UtilityHelper.Serializer(new Dictionary<string, string> { { "key", "value" } });
+            var obj = UtilityHelper.Deserializer(str);
+
+        }
+    }
+    interface IUtilityHelper
+    {
+        string Serializer(object data);
+        object Deserializer(string value);
+
+    }
+
+    static class UtilityHelper
+    //static class can't implement interfaces 
+    //: IUtilityHelper
+    {
+        //public UtilityHelper()
+        //{
+        //static class can't have constructor 
+        //}
+        //static class can't have variable
+        //private string SomeVariable;
+        public static string Serializer(object data)
+        {
+            string strValue = string.Empty;
+            try
+            {
+                strValue = JsonConvert.SerializeObject(data);
+            }
+            catch (Exception)
+            {
+                return default;
+            }
+            return strValue;
+        }
+
+        public static object Deserializer(string value)
+        {
+            try
+            {
+                 return JsonConvert.DeserializeObject(value);
+            }
+            catch (Exception)
+            {
+                return default;
+            }
+        }
+
+        public static bool IsValidEmail(this string email)
+        {
+            // Simple email validation logic
+            return email.Contains("@") && email.Contains(".");
+        }
+    }
+}
